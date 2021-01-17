7537ee34fa7bdf5863ea864bca69b85d9cae0d7a
Theresa <TreeGee73@gmail.com>
2021-01-17 13:32:40 -0700

    initial setup - server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 5201;
+
+const app = express();
+
+// Middleware
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Database Connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+// Routes
+require('../routes/api')(app);
+require('../routes/views')(app);
+
+// Start the server and begin listening for client requests
+app.listen(PORT, function () {
+    // Server log to indicate the server has started running
+    console.log(`Server is listening on: http://localhost:${PORT}`);
+});
