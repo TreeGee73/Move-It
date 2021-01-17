2a057e4f4921cfbaf61fb5c36eff15ad1b184f05
Theresa <TreeGee73@gmail.com>
2021-01-17 16:44:46 -0700

    debugging

--- models/workout.js
+++ models/workout.js
@@ -3,29 +3,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create the database schema / model workout properties
-const workoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: function () {
-      return new Date();
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: function () {
+            return new Date();
+        },
     },
-  },
-  exercises: [
-    {
-      type: { type: String, trim: true, required: "Enter an exercise type:" },
-      name: { type: String, trim: true, required: "Enter an exercise name:" },
-      duration: {
-        type: Number,
-        required: "Enter an exercise duration in minutes:",
-      },
-      weight: { type: Number },
-      reps: { type: Number },
-      sets: { type: Number },
-      distance: { type: Number },
+    exercises: [
+        {
+            type: { type: String, trim: true, required: "Enter an exercise type:" },
+            name: { type: String, trim: true, required: "Enter an exercise name:" },
+            duration: {
+                type: Number,
+                required: "Enter an exercise duration in minutes:",
+            },
+            weight: { type: Number },
+            reps: { type: Number },
+            sets: { type: Number },
+            distance: { type: Number },
+        },
+    ],
+},
+  {
+    toJSON: {
+      virtuals: true,
     },
-  ],
+  }
+);
+
+WorkoutSchema.virtual('totalDuration').get(function () {
+  return this.exercises.reduce((sum, exercise) => sum + exercise.duration, 0);
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
-
-module.exports = Workout;
+const workout = mongoose.model('WorkoutSchema', WorkoutSchema);
+module.exports = workout;
--- routes/api.js
+++ routes/api.js
@@ -2,84 +2,45 @@
 const Workout = require("../models/workout.js");
 
 module.exports = function (app) {
-  // Creates a new workout based on client data
-  app.post("/api/workouts", function (req, res) {
-    Workout.create({})
-      .then(function (dbWorkout) {
-        res.json(dbWorkout);
-      })
-      .catch(function (err) {
-        res.json(err);
-      });
+  app.get("/api/workouts", async function (req, res) {
+    try {
+      const workouts = await Workout.find({});
+      res.json(workouts);
+    } catch (err) {
+      res.status(500).end();
+    }
   });
 
-  // Adds a new exercise
-  app.put("/api/workouts/:id", function ({ body, params }, res) {
-    Workout.findByIdAndUpdate(
-      params.id,
-      //
-      { $push: { exercises: body } },
-      { new: true, runValidators: true }
-    )
-      .then(function (dbWorkouts) {
-        res.json(dbWorkouts);
-      })
-      .catch(function (err) {
-        res.json(err);
-      });
+  app.get("/api/workouts/range", async function (req, res) {
+    try {
+      const data = await Workout.find({});
+      res.json(data);
+    } catch (err) {
+      res.status(500).end();
+    }
   });
 
-  // Get and display all exercises for the day and display with a total workout duration
-  app.get("/api/workouts", function (req, res) {
-    // Uses 'aggrigate' to dynamically create 'total duration' field and make it equal to the sum of the durations of all exercises
-    Workout.aggregate([
-      { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
-    ])
-      .then(function (dbWorkouts) {
-        res.json(dbWorkouts);
-      })
-      .catch(function (err) {
-        res.json(err);
-      });
+  app.put("/api/workouts/:id", async function ({ params, body }, res) {
+    try {
+      const updatedWorkout = await Workout.findByIdAndUpdate(
+        params.id,
+        {
+          $push: { exercises: body },
+        },
+        { new: true, runValidators: true }
+      );
+      res.json(updatedWorkout);
+    } catch (err) {
+      res.status(500).end();
+    }
   });
 
-  // Gets all exercises and workouts for a set period
-  app.get("/api/workouts/range", function (req, res) {
-    // Uses 'aggrigate' to dynamically create 'total duration' field and make it equal to the sum of the durations of all exercises
-    Workout.aggregate([
-      { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
-    ])
-      // Sorts in decending order by exercise durtion (from line 47)
-      .sort({ _id: -1 })
-      // Limits the range to seven days
-      .limit(7)
-      .then(function (dbWorkouts) {
-        console.log(dbWorkouts);
-      })
-      .catch(function (err) {
-        res.json(err);
-      });
-  });
-
-  // Posts the exercises and workouts for the selected period
-  app.post("/api/workouts/range", function (req, res) {
-    Workout.create({})
-      .then(function (dbWorkout) {
-        res.json(dbWorkout);
-      })
-      .catch(function (err) {
-        res.json(err);
-      });
-  });
-
-  // Deletes the exercise/workout from the database
-  app.delete("/api/workouts", function ({ body }, res) {
-    Workout.findByIdAndDelete(body.id)
-      .then(function () {
-        res.json(true);
-      })
-      .catch(function (err) {
-        res.json(err);
-      });
+  app.post("/api/workouts", async function (req, res) {
+    try {
+      const newWorkout = await Workout.create({});
+      res.json(newWorkout);
+    } catch (err) {
+      res.status(500).end();
+    }
   });
 };
